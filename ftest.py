d75e0a46149546c706f2f7ce18cf88efa9e5828d
BitFloyd <sby.jcub@gmail.com>
2018-01-26 14:16:10 -0500

    Test face detection

--- ftest.py
+++ ftest.py
@@ -3,11 +3,20 @@ import numpy as np
 from classico.faces import FaceDetector as FD
 import matplotlib.pyplot as plt
 import os
+import sys
 
-fd = FD()
+fd = FD(face_cascade=os.path.join('..','data_folder','haarcascades','haarcascade_frontalface_default.xml'),
+        eye_cascade=None,
+        eyes=False)
 
-dir = os.path.join('data','frames','webcam2')
-dir_results = os.path.join('data','frames','webcam2_results')
+dir = os.path.join('..','data_folder','frames','webcam1_frames')
+assert os.path.exists(dir),'Directory '+dir+' does not exist'
+
+
+dir_results = os.path.join('..','data_folder','frames','webcam1_results')
+
+if(not os.path.exists(dir_results)):
+    os.makedirs(dir_results)
 
 for i in sorted(os.listdir(dir)):
     img = cv2.imread(os.path.join(dir,i))
